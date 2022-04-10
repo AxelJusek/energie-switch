553264eabbcea6537ebc3de353e4a861c2e5ee19
Axel Jusek <jusek@gmx.net>
2022-04-10 16:07:37 +0200

    Minor change for readability.

--- src/main/java/de/axeljusek/servertools/energie/commandline/ParameterKommandozeile.java
+++ src/main/java/de/axeljusek/servertools/energie/commandline/ParameterKommandozeile.java
@@ -34,21 +34,27 @@
 package de.axeljusek.servertools.energie.commandline;
 
 public enum ParameterKommandozeile {
-  HilfeAnzeigen("-h", false, null, "", "Zeigt diesen Hilfetext an."), HilfeAnzeigen2("-help", false,
-      null, "", "siehe -h"), SchaltenEinerDoseGewuenschterZustand("-d", true, null, "true | false",
-          "Der gewuenschte Schaltzustand: True steht fuer ein und False fuer aus."), SchaltenEinerDoseDosenNr(
-              "-s", true, ParameterKommandozeile.SchaltenEinerDoseGewuenschterZustand,
-              "0 | 1 | 2 | 3", "Die Nummer der Dose die geschaltet werden soll."), IpAdresseAngeben(
-                  "-ip", true, null, "z.B. 192.168.0.254",
-                  "Die IP-Adresse über welche die Steckdosenleiste erreicht wird."), PortNrAngeben(
-                      "-port", true, null, "z.B. 5000",
-                      "Die Port-Nummer auf welche die Steckdosenleiste hört."), ProfileNr(
-                          "-profile", true, null, "",
-                          "Zur Zeit noch nicht realisiert."), ZustandEinerDoseAbfragen("-z", true,
-                              null, "-z 0",
-                              "Abfragen des Zustands der gegebenen Dose."), PasswordAngeben(
-                                  "-passwd", true, null, "       1",
-                                  "Das Passwort fuer die Steckdosenleiste.");
+  HilfeAnzeigen("-h", false, null, "", "Zeigt diesen Hilfetext an."),
+
+  HilfeAnzeigen2("-help", false, null, "", "siehe -h"),
+
+  SchaltenEinerDoseGewuenschterZustand("-d", true, null, "true | false",
+      "Der gewuenschte Schaltzustand: True steht fuer ein und False fuer aus."),
+
+  SchaltenEinerDoseDosenNr("-s", true, ParameterKommandozeile.SchaltenEinerDoseGewuenschterZustand,
+      "0 | 1 | 2 | 3", "Die Nummer der Dose die geschaltet werden soll."),
+
+  IpAdresseAngeben("-ip", true, null, "z.B. 192.168.0.254",
+      "Die IP-Adresse über welche die Steckdosenleiste erreicht wird."),
+
+  PortNrAngeben("-port", true, null, "z.B. 5000",
+      "Die Port-Nummer auf welche die Steckdosenleiste hört."),
+
+  ProfileNr("-profile", true, null, "", "Zur Zeit noch nicht realisiert."),
+
+  ZustandEinerDoseAbfragen("-z", true, null, "-z 0", "Abfragen des Zustands der gegebenen Dose."),
+
+  PasswordAngeben("-passwd", true, null, "       1", "Das Passwort fuer die Steckdosenleiste.");
 
 
   private String parameter = "";
