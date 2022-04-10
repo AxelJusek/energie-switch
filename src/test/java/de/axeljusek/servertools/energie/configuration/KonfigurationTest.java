89535fef8d2bba733f36b1e3e14805b7601adbf7
Axel Jusek <jusek@gmx.net>
2022-04-10 12:26:55 +0200

    delete old test case.

--- src/test/java/de/axeljusek/servertools/energie/configuration/KonfigurationTest.java
+++ src/test/java/de/axeljusek/servertools/energie/configuration/KonfigurationTest.java
@@ -4,15 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class KonfigurationTest {
-
+  private String configFilename = "konfiguration.conf";
+  
   @Test
   void testGetInstance() {
-    fail("Not yet implemented");
+    Konfiguration conf = Konfiguration.getInstance();
+    assertNotNull(conf, "Default File wurde angelegt.");
   }
 
   @Test
   void testGetInstanceForConfigFilename() {
-    fail("Not yet implemented");
+    Konfiguration conf =  Konfiguration.getInstanceForConfigFilename(configFilename);
+    assertNotNull(conf, "Starten mit Konfig-File.");
   }
 
   @Test
@@ -22,7 +25,14 @@ class KonfigurationTest {
 
   @Test
   void testGetValueForKey() {
-    fail("Not yet implemented");
+    Konfiguration conf = Konfiguration.getInstanceForConfigFilename(configFilename);
+    assertNotNull(conf, "Default File wurde angelegt.");
+
+    String port = conf.getValueForKey("port");
+    assertEquals("5000", port, "Prüfen ob der Wert auch stimmt.");
+
+    String ip = conf.getValueForKey("ip_address");
+    assertEquals("192.168.178.111", ip, "Prüfen ob die IP stimmt.");
   }
 
 }
